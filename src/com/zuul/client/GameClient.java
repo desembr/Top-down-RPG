fb64044dd7c9a171f19a8949bd46d4e48b5533e9
Tom Bjurenlind <tombjurenlind@gmail.com>
2018-02-09 16:58:27 +0100

    Added some files

---
+++ src/com/zuul/client/GameClient.java
@@ -0,0 +1,10 @@
+package com.zuul.client;
+
+/**
+ * Interface between players and the server.
+ * @author  Tom Bjurenlind, Jan Rasmussen, Christer Sonesson, Emir Zivcic
+ * @version 1.0
+ */
+public class GameClient {
+
+}
---
+++ src/com/zuul/server/command/use.java
@@ -0,0 +1,10 @@
+package com.zuul.server.command;
+
+/**
+ * Use-item command.
+ * @author  Tom Bjurenlind, Jan Rasmussen, Christer Sonesson, Emir Zivcic
+ * @version 1.0
+ */
+public class Use {
+
+}
