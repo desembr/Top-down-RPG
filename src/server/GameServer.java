b1f6a51437f1de1f26ce8e5d94ca50f12806b382
Emir Zivcic <emir-s@hotmail.se>
2018-02-15 10:46:06 +0100

    Not tested

--- src/server/GameServer.java
+++ src/server/GameServer.java
@@ -1,6 +1,11 @@
 package server;
 
-import java.net.DatagramSocket;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 
 /**
@@ -13,25 +18,60 @@ public class GameServer extends Thread
 {
 	//private UserInterface gui;
 	private GameEngine engine;
-	private DatagramSocket serverSocket;
+	//private DatagramSocket serverSocket;
+	private ServerSocket server;
+	private Socket clientSocket;
+	
+	private ObjectOutputStream out;
+	private ObjectInputStream in;
+	
+	private static final int PORT = 8989;
 
     /**
      * Create the game and initialize its internal map.
      */
-    public GameServer() 
-    {
+    public GameServer() throws IOException {
 		engine = new GameEngine();
-		//gui = new UserInterface(engine);
-		//engine.setGUI(gui);
+	
+		try{
+			server = new ServerSocket(PORT);
+			
+			while(true){
+				try{
+					//Wait for Connection
+					connection();
+					//Streams for connection
+					streams();
+					//Communication
+					receiveCommand();
+					
+				}catch(EOFException e){
+					e.printStackTrace();
+				}finally{
+					//Close connection
+					close();
+				}
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
     }
     
     /**
      * Listen for commands from a client.
-     * @return The received command to process.
+     * 
      */
     private String receiveCommand() {
-    	//TODO: implement communication with clients.
-    	return "";
+    	String command = "";
+    		try{
+    			command = (String) in.readObject();
+    		}catch(IOException e){
+    			System.out.println("Unknown Message");
+    		}catch(ClassNotFoundException e){
+    			System.out.println("Unknown Format");
+    		}
+    	return command;
     }
     
     /**
@@ -43,6 +83,8 @@ public class GameServer extends Thread
     private void sendResponse(String responseMessage) {
     	//TODO: implement communication with clients.
     	List<Player> players = engine.getPlayers();
+    	
+    	
     }
     
     /**
@@ -56,10 +98,36 @@ public class GameServer extends Thread
     	}
     }
     
+    private void streams() throws IOException {
+    	out = new ObjectOutputStream(clientSocket.getOutputStream());
+    	out.flush();
+    	
+    	in = new ObjectInputStream(clientSocket.getInputStream());
+    	System.out.println("Server ready to exchange data\n");
+    }
+    
+    private void connection() throws IOException {
+    	System.out.println("Waiting for player to connect!\n");
+    	clientSocket = server.accept();
+    	System.out.println(" Player " + clientSocket.getInetAddress().getHostName() + " Connected\n");
+    }
+    
+    private void close() {
+    	System.out.println("Closing Connection\n");
+    	try{
+    		out.close();
+    		in.close();
+    		clientSocket.close();
+    	}catch(IOException e){
+    		e.printStackTrace();
+    	}
+    }
+    
     /**
      * Entry point to server program.
+     * @throws IOException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
     	new GameServer();
     }
 }
