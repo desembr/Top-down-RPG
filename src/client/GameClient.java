0361a012f9ed9ca2daa02c256ff66b188d840507
Tom Bjurenlind <tombjurenlind@gmail.com>
2018-02-17 17:20:57 +0100

    Updated uml diagrams

--- src/client/GameClient.java
+++ src/client/GameClient.java
@@ -1,7 +1,5 @@
 package client;
 
-import server.GameServer;
-
 /**
  * Main class on the client-side, which creates core objects and stores state.
  * @author  Tom Bjurenlind, Jan Rasmussen, Christer Sonesson, Emir Zivcic
@@ -11,15 +9,10 @@ public class GameClient {
 	private UserInterface gui;
 	Client client;
 	
-	//GameServer server; // för att underlätta arbetet om man jobbar lokalt
-	
 	/**
      * Constructor of this class.
      */
 	public GameClient() {
-		
-		//server = new GameServer(); // för att underlätta arbetet om man jobbar lokalt
-		
 		client = new Client();
 		gui = new UserInterface(client);
 		client.addObserver(gui);
